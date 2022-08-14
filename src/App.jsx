80ccc0c2fef4b75dc65b6792741195243705771f
Eliza Cantwell <elizaofthebay@gmail.com>
2022-08-13 17:02:11 -0700

    Make function to divide character groups depending on destination
    Given one text coming down off a marquee and another text going up,
    create a counter that tracks how many of each character are coming down,
    going up, and staying up.

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import './App.css'
 
 function countChar(text) {
   // Given an input text block, return a object counting the characters it contains. 
-  // Ignore characters not included in marquee letters alphabet and remove characters 
-  // that appear zero times from object. 
+  // Ignore characters not included in marquee letters alphabet.
 
   const charCounter = {}
   const alpha = "abcdefghijklmnopqrstuvwxyz0123456789.,:'&!"
@@ -22,22 +21,22 @@ function countChar(text) {
     };
   };
 
-// remove all chars from display that are not in text 
-  for (let key of Object.keys(charCounter)) {
-    if (charCounter[key] === 0) {
-      delete charCounter[key];
-    };
-  };
+// remove all chars from display that are not in text --> now done in AlphaDisplay with filter  
+  // for (let key of Object.keys(charCounter)) {
+  //   if (charCounter[key] === 0) {
+  //     delete charCounter[key];
+  //   };
+  // };
 
   return charCounter
 };
 
 function CharDisplay(props) {
   return <h1>{props.char}: {props.count}</h1>;
-}
+};
 
-function FullAlpha(props) {
-  const charCount = Object.entries(props.counts).map(
+function AlphaDisplay(props) {
+  const charCount = Object.entries(props.counts).filter(x => x[1] !== 0).map(
     x => <div><CharDisplay char={x[0]} count={x[1]}/></div>
   )
 
@@ -46,27 +45,77 @@ function FullAlpha(props) {
       {charCount}
     </div>
   )
-}
+};
+
+function countDifference(text1, text2) {
+  const charsGoingUp = {};       // dict of chars going up on marquee
+  const charsComingDown = {};    // dict of chars coming off marquee
+  const charsStayingUp = {};     // dict of chars staying on marquee
+
+  let dict1 = countChar(text1['text1']);
+  let dict2 = countChar(text2['text2']);
+
+  for (let [key, value] of Object.entries(dict2)) {
+    if (value > dict1[key]) {                   
+      charsGoingUp[key] = value - dict1[key];
+      charsStayingUp[key] = dict1[key]
+    };
+
+    if (value < dict1[key]) {
+      charsComingDown[key] = dict1[key] - value;
+      charsStayingUp[key] = dict2[key]
+
+    };
+
+    if (value != 0) {
+      if (value === dict1[key]) {
+        charsStayingUp[key] = value;
+      };  
+    };
+  }
+
+  return {
+    "charsGoingUp": Object.entries(charsGoingUp),
+    "charsComingDown": Object.entries(charsComingDown),
+    "charsStayingUp": Object.entries(charsStayingUp),
+  }
+ 
+};
+
+
+
 
 function App() {
-  const [text, setText] = useState("");
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("");
   // const element = <h1>Hello, world!</h1>;
   //       {element}
   //       {JSON.stringify(countChar(text))}
 
   return (
     <div className="App">
+      <h1>Current Marquee</h1>
       <input
-        type="text"
-        value={text}
-        onChange={(e)=>setText(e.target.value)}
+        type="text1"
+        value={text1}
+        onChange={(e)=>setText1(e.target.value)}
       />
+      <AlphaDisplay counts={countChar(text1)} />
 
-      <FullAlpha counts={countChar(text)} />
+      <h1>New Marquee</h1>
+      <input
+        type="text2"
+        value={text2}
+        onChange={(e)=>setText2(e.target.value)}
+      />
+      <AlphaDisplay counts={countChar(text2)} />
+
+      <h1>Difference</h1>
+      <h2></h2>
 
     </div>
   )
-}
+};
 
 export default App
 
